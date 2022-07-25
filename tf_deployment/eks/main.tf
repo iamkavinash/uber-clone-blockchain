4b72bb1e2901a3010519966eb2be2c0e3322c989
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-07-25 15:26:33 -0500

    updating

--- tf_deployment/eks/main.tf
+++ tf_deployment/eks/main.tf
@@ -11,81 +11,81 @@ resource "aws_eks_cluster" "elliotteks" {
     subnet_ids = [for id in var.public_eks_subnets: id.id]
   }
 
-#   depends_on = [
-#   aws_iam_role.eks-iam-role,
-#  ]
+  depends_on = [
+  aws_iam_role.eks-iam-role-,
+ ]
 }
 
 
-# resource "aws_iam_role" "eks-iam-role" {
-#   name = "eks-cluster-"
+resource "aws_iam_role" "eks-iam-role" {
+  name = "eks-cluster-"
 
-#   assume_role_policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "eks.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# POLICY
-# }
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
 
-# resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#  role    = aws_iam_role.eks-iam-role.name
-# }
-# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#  role    = aws_iam_role.eks-iam-role.name
-# }
-
-
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ role    = aws_iam_role.eks-iam-role.name
+}
 
 
-# resource "aws_iam_role" "workernodes" {
-#   name = "eks-node-group"
+
+
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-"
  
-#   assume_role_policy = jsonencode({
-#    Statement = [{
-#     Action = "sts:AssumeRole"
-#     Effect = "Allow"
-#     Principal = {
-#      Service = "ec2.amazonaws.com"
-#     }
-#    }]
-#    Version = "2012-10-17"
-#   })
-#  }
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
+    }
+   }]
+   Version = "2012-10-17"
+  })
+ }
  
-#  resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role    = aws_iam_role.workernodes.name
-#  }
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.workernodes.name
+ }
  
-#  resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role    = aws_iam_role.workernodes.name
-#  }
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.workernodes.name
+ }
  
-#  resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
-#   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-#   role    = aws_iam_role.workernodes.name
-#  }
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.workernodes.name
+ }
  
-#  resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role    = aws_iam_role.workernodes.name
-#  }
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.workernodes.name
+ }
 
   resource "aws_eks_node_group" "worker-node-group" {
   cluster_name  = aws_eks_cluster.elliotteks.name
-  node_group_name = "elliotts-eks-workernodes"
+  node_group_name = "elliotts-eks-workernodes-"
   node_role_arn  = aws_iam_role.workernodes.arn
   subnet_ids   = [for id in var.public_eks_subnets: id.id]
   instance_types = ["t2.small"]
