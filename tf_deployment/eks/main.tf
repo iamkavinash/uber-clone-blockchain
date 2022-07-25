d960dbda027c4de27b7c1dbda7fa6191f0949bf7
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-07-25 14:56:29 -0500

    updating

--- tf_deployment/eks/main.tf
+++ tf_deployment/eks/main.tf
@@ -17,71 +17,71 @@ resource "aws_eks_cluster" "elliotteks" {
 }
 
 
-resource "aws_iam_role" "eks-iam-role" {
-  name = "eks-cluster-"
+# resource "aws_iam_role" "eks-iam-role" {
+#   name = "eks-cluster-"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
+#   assume_role_policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "eks.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
- role    = aws_iam_role.eks-iam-role.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
- role    = aws_iam_role.eks-iam-role.name
-}
-
-
+# resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#  role    = aws_iam_role.eks-iam-role.name
+# }
+# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#  role    = aws_iam_role.eks-iam-role.name
+# }
 
 
-resource "aws_iam_role" "workernodes" {
-  name = "eks-node-group"
+
+
+# resource "aws_iam_role" "workernodes" {
+#   name = "eks-node-group"
  
-  assume_role_policy = jsonencode({
-   Statement = [{
-    Action = "sts:AssumeRole"
-    Effect = "Allow"
-    Principal = {
-     Service = "ec2.amazonaws.com"
-    }
-   }]
-   Version = "2012-10-17"
-  })
- }
+#   assume_role_policy = jsonencode({
+#    Statement = [{
+#     Action = "sts:AssumeRole"
+#     Effect = "Allow"
+#     Principal = {
+#      Service = "ec2.amazonaws.com"
+#     }
+#    }]
+#    Version = "2012-10-17"
+#   })
+#  }
  
- resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role    = aws_iam_role.workernodes.name
- }
+#  resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#   role    = aws_iam_role.workernodes.name
+#  }
  
- resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role    = aws_iam_role.workernodes.name
- }
+#  resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#   role    = aws_iam_role.workernodes.name
+#  }
  
- resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
-  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  role    = aws_iam_role.workernodes.name
- }
+#  resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+#   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+#   role    = aws_iam_role.workernodes.name
+#  }
  
- resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role    = aws_iam_role.workernodes.name
- }
+#  resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   role    = aws_iam_role.workernodes.name
+#  }
 
   resource "aws_eks_node_group" "worker-node-group" {
   cluster_name  = aws_eks_cluster.elliotteks.name
@@ -104,16 +104,16 @@ resource "aws_iam_role" "workernodes" {
 
 
 
-#SERVICE ACCOUNT CONFIG 
+# #SERVICE ACCOUNT CONFIG 
 
 
-# #correct
-# data "tls_certificate" "ekstls" {
-#   url = aws_eks_cluster.elliotteks.identity[0].oidc[0].issuer
-# }
+# # #correct
+# # data "tls_certificate" "ekstls" {
+# #   url = aws_eks_cluster.elliotteks.identity[0].oidc[0].issuer
+# # }
 
-# resource "aws_iam_openid_connect_provider" "eksoidc" {
-#   client_id_list  = ["sts.amazonaws.com"]
+# # resource "aws_iam_openid_connect_provider" "eksoidc" {
+# #   client_id_list  = ["sts.amazonaws.com"]
 #   thumbprint_list = [data.tls_certificate.ekstls.certificates[0].sha1_fingerprint]
 #   url             = aws_eks_cluster.elliotteks.identity[0].oidc[0].issuer
 # }
