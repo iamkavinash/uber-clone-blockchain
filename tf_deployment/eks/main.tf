8543d24b2a88751d2d18bcc83e7e6c2932c80e55
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-07-25 20:08:56 -0500

    updated

--- tf_deployment/eks/main.tf
+++ tf_deployment/eks/main.tf
@@ -9,12 +9,38 @@ resource "aws_eks_cluster" "elliotteks" {
 
   vpc_config {
     subnet_ids = [for id in var.public_eks_subnets: id.id]
+    security_group_ids =[aws_security_group.public_sg.id]
   }
 
   depends_on = [
   aws_iam_role.eks-iam-role,
  ]
 }
+
+
+
+
+
+resource "aws_security_group" "public_sg" {
+
+  name   = "sandbox-eks-open"
+  vpc_id = aws_vpc.elliott-k8s-vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 65000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 
 resource "aws_iam_role" "eks-iam-role" {
